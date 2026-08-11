a37f5478c21f8111d4397ce7043e2ea9df8dc666
roni <rs0748edu.turku.fi>
2026-08-11 10:30:10 +0300

    valmis

--- ostoslista.py
+++ ostoslista.py
@@ -3,25 +3,25 @@ lista =[ {'nimi':'leipä', 'määrä':2 }, {'nimi':'maito', 'määrä':5},{'nimi
 while True:
     
     print("1. näytä ostos lista")
-    print("2. Lisää")
+    print("2. Lisää")#kaikki eri moodit jota voi tehdä
     print("3. lopeta")
     print("4. poista tuote")
     vaiht = input("")
 
     if vaiht =="1":
-        for ostos in lista:
+        for ostos in lista:#vaihtoetho 1 näytää listan
             print(ostos['nimi']+' '+ str(ostos['määrä'])) 
             
 
     elif vaiht =="2":
         tuot = input("mitä haluat lisätä ")
-        määrä =input("kuinka monta ")
-        tuote = {'nimi': tuot, 'määrä':määrä}
+        määrä =int(input("kuinka monta "))#vaihtoehto 2 lisää listaan tavaraa
+        tuote = {'nimi': tuot, 'määrä':määrä}#lisätään tuoteen nimi ja määrä muutujaan
         lista.append(tuote)
     elif vaiht=="3":
         break
     elif vaiht=="4":
-        for tuote in enumerate(lista):
+        for tuote in enumerate(lista):#saadan listan numerot
             print(tuote)
-        nimi =int(input("mikä poistetaan"))
-        lista.pop(nimi)
+        nimi =int(input("mikä poistetaan"))#poistettavan numero
+        lista.pop(nimi)#poistetaan poistettava asia
